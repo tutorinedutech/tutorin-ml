39c213a3d47fe62bc47500edc0dbdd785e686c0a
Zulfikar03 <zulfikarirham02@gmail.com>
2024-06-02 10:27:59 +0700

    add class data prerocess in data_loader.py

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,15 +1,12 @@
 import pandas as pd
 import tensorflow as tf
-from src.utils.helper_function import split_data, load_data
 
 class DataLoader:
-    def __init__(self, df, batch_size, num_batches, train_split, buffer_size, AUTOTUNE):
+    def __init__(self, df,  train_split, buffer_size):
         self.df = df
-        self.batch_size = batch_size
-        self.num_batches = num_batches
         self.train_split = train_split
         self.buffer_size = buffer_size
-        self.AUTOTUNE = AUTOTUNE
+
     def get_tensor_data(self):
         X_df = self.df.drop('Skor Label', axis=1)
         y_df = self.df['Skor Label']
@@ -18,9 +15,25 @@ class DataLoader:
     
     def get_prefetch_data(self, ds):
         ds = ds.map(lambda prompt, kriteria, y: {'Prompt' : prompt, 'Kriteria' : kriteria, 'label':y})
-        return ds.prefetch(1)
+        return ds.prefetch(1)    
+   
+    def split_data(self, ds):
+        train_ds_size = int(self.train_split*self.df.shape[1])
+        remain_ds_size = int((1-self.train_split)*self.df.shape[1])
+        ds = ds.shuffle(self.buffer_size)
+        train_ds = ds.take(train_ds_size)
+        remain_ds = ds.skip(train_ds_size)
+        val_ds = remain_ds.take(remain_ds_size)
+        test_ds = remain_ds.skip(remain_ds_size)
+        return train_ds, val_ds, test_ds
     
-    def change_range(self, x):
+class Preprocess:
+     def __init__(self, batch_size, num_batches, AUTOTUNE):
+        self.batch_size = batch_size
+        self.num_batches = num_batches
+        self.AUTOTUNE = AUTOTUNE
+
+     def change_range(self, x):
         x = (x/2.5)-2
         return x
     
@@ -36,13 +49,3 @@ class DataLoader:
         ds = ds.take(self.num_batches)
         ds = ds.prefetch(self.AUTOTUNE)
         return ds
-
-    def split_data(self, ds):
-        train_ds_size = int(self.train_split*self.df.shape[1])
-        remain_ds_size = int((1-self.train_split)*self.df.shape[1])
-        ds = ds.shuffle(self.buffer_size)
-        train_ds = ds.take(train_ds_size)
-        remain_ds = ds.skip(train_ds_size)
-        val_ds = remain_ds.take(remain_ds_size)
-        test_ds = remain_ds.skip(remain_ds_size)
-        return train_ds, val_ds, test_ds
